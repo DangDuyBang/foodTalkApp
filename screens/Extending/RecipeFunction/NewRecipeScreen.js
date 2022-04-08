b3c81bc9e90f291dce44579bd29db08823e967b9
Đặng Duy Bằng <81517068+DangDuyBang@users.noreply.github.com>
2022-04-08 10:47:08 +0700

    bottomsheet upload photo

--- screens/Extending/RecipeFunction/NewRecipeScreen.js
+++ screens/Extending/RecipeFunction/NewRecipeScreen.js
@@ -7,11 +7,67 @@ import ProcessAdd from '../../../components/ProcessAdd'
 import ProcessShow from '../../../components/ProcessShow'
 import IngredientShow from '../../../components/IngredientShow'
 import useNewReceipt from './hooks/useNewReceipt'
+import Animated from 'react-native-reanimated';
+import BottomSheet from 'reanimated-bottom-sheet';
 
 const NewRecipeScreen = ({ navigation }) => {
-    const { processList, ingredientList,uri, handleAddProcess, handleAddIngrdient, handleDeleteProcess, handleDeleteIngredient, openImagePickerAsync} = useNewReceipt()
+    const renderInner = () => (
+        <View style={styles.panel}>
+            <View onPress={() => bs.current.snapTo(1)} style={{ alignItems: 'center' }}>
+                <View style={{ height: 4, width: 65, borderRadius: 100, backgroundColor: color.textIconSmall, marginTop: 15 }} />
+                <Text onPress={() => bs.current.snapTo(1)}
+                    style={{
+                        fontFamily: 'Roboto',
+                        fontSize: 22,
+                        fontWeight: 'bold',
+                        color: color.textGray,
+                        marginBottom: 10
+                    }}>
+                    Upload Photo
+                </Text>
+            </View>
+            <TouchableOpacity>
+                <View style={styles.frameOptionSetting}>
+                    <Text style={styles.optionSetting}>Take Photo</Text>
+                </View >
+            </TouchableOpacity>
+            <TouchableOpacity onPress={openImagePickerAsync}>
+                <View style={styles.frameOptionSetting}>
+                    <Text style={styles.optionSetting}>Choose From Library</Text>
+                </View>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => bs.current.snapTo(1)}>
+                <View style={styles.frameOptionSetting}>
+                    <Text style={styles.optionSetting}>Cancel</Text>
+                </View>
+            </TouchableOpacity>
+        </View>
+    );
+
+    const renderHeader = () => (
+        <View style={styles.header}>
+            <View style={styles.panelHeader}>
+                <View style={styles.panelHandle}></View>
+            </View>
+        </View>
+    );
+
+    const bs = React.createRef();
+    const fall = new Animated.Value(1);
+
+    const { processList, ingredientList, uri, handleAddProcess, handleAddIngrdient, handleDeleteProcess, handleDeleteIngredient, openImagePickerAsync } = useNewReceipt()
     return (
         <View style={styles.container}>
+            <BottomSheet
+                ref={bs}
+                snapPoints={['31%', -300]}
+                borderRadius={10}
+                renderContent={renderInner}
+                renderHeader={renderHeader}
+                initialSnap={1}
+                callbackNode={fall}
+                enabledGestureInteraction={true}
+            />
             <View style={styles.topView}>
                 <View style={styles.topLeftView}>
                     <TouchableOpacity onPress={() => { navigation.navigate('RecipeAttached') }}>
@@ -23,46 +79,54 @@ const NewRecipeScreen = ({ navigation }) => {
                     <Ionicons name='checkmark-sharp' size={35} color={color.primary}></Ionicons>
                 </TouchableOpacity>
             </View>
-            <ScrollView>
-                <View style={styles.body}>
-                    <View style={styles.imageFoodFrame}  >
-                        <TouchableOpacity onPress={openImagePickerAsync}>
-                            {!uri?<Ionicons name='camera-outline' size={35} color={color.primary}></Ionicons>:
-                            <Image source = {uri} style = {styles.image}/>}
-                        </TouchableOpacity>
+            <Animated.View
+                style={{
+                    margin: 0,
+                    opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)),
+                }}
+            >
+                <ScrollView>
+                    <View style={styles.body}>
+                        <View style={styles.imageFoodFrame}  >
+                            <TouchableOpacity onPress={() => bs.current.snapTo(0)}>
+                                {/* onPress={openImagePickerAsync} */}
+                                {!uri ? <Ionicons name='camera-outline' size={35} color={color.primary}></Ionicons> :
+                                    <Image source={uri} style={styles.image} />}
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={styles.ingredientTittle}>Food Name</Text>
+                        <TextInput
+                            placeholder='Type name of food'
+                            maxLength={20}
+                            style={{
+                                fontFamily: 'Roboto',
+                                borderBottomWidth: 0.5,
+                                paddingVertical: 10,
+                                fontSize: 16,
+                                paddingHorizontal: 5
+                            }}
+                        />
+                        <Text style={styles.ingredientTittle}>Ingredients</Text>
+                        <View>
+                            {
+                                ingredientList.map((item, index) => {
+                                    return <IngredientShow key={index} ingredientAn={item} onDeleteIngredient={() => handleDeleteIngredient(index)} />
+                                })
+                            }
+                        </View>
+                        <IngredientAdd onAddIngredient={handleAddIngrdient} />
+                        <Text style={styles.ingredientTittle}>Process</Text>
+                        <View>
+                            {
+                                processList.map((item, index) => {
+                                    return <ProcessShow key={index} step={item} indexNumber={index + 1} onDeleteProcess={() => handleDeleteProcess(index)} />
+                                })
+                            }
+                        </View>
+                        <ProcessAdd onAddProcess={handleAddProcess} />
                     </View>
-                    <Text style={styles.ingredientTittle}>Food Name</Text>
-                    <TextInput
-                        placeholder='Type name of food'
-                        maxLength={20}
-                        style={{
-                            fontFamily: 'Roboto',
-                            borderBottomWidth: 0.5,
-                            paddingVertical: 10,
-                            fontSize: 16,
-                            paddingHorizontal: 5
-                        }}
-                    />
-                    <Text style={styles.ingredientTittle}>Ingredients</Text>
-                    <View>
-                        {
-                            ingredientList.map((item, index) => {
-                                return <IngredientShow key={index} ingredientAn={item} onDeleteIngredient={() => handleDeleteIngredient(index)} />
-                            })
-                        }
-                    </View>
-                    <IngredientAdd onAddIngredient={handleAddIngrdient} />
-                    <Text style={styles.ingredientTittle}>Process</Text>
-                    <View>
-                        {
-                            processList.map((item, index) => {
-                                return <ProcessShow key={index} step={item} indexNumber={index + 1} onDeleteProcess={() => handleDeleteProcess(index)} />
-                            })
-                        }
-                    </View>
-                    <ProcessAdd onAddProcess={handleAddProcess} />
-                </View>
-            </ScrollView>
+                </ScrollView>
+            </Animated.View>
         </View>
     )
 }
@@ -100,8 +164,10 @@ const styles = StyleSheet.create({
         paddingTop: 10,
     },
     imageFoodFrame: {
-        width: '100%',
+        width: 370,
         height: 250,
+        borderRadius: 20,
+        borderWidth: 1,
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -113,7 +179,34 @@ const styles = StyleSheet.create({
         fontWeight: 'bold'
     },
     image: {
-        resizeMode: 'contain',
-        height: '100%'
+        resizeMode: 'stretch',
+        width: 370,
+        height: 250,
+        borderRadius: 20,
+    },
+    panel: {
+        backgroundColor: color.background,
+        borderTopRightRadius: 40,
+        borderTopLeftRadius: 40,
+        borderLeftWidth: 1,
+        borderRightWidth: 1,
+        borderTopWidth: 1,
+    },
+    optionSetting: {
+        fontFamily: 'Roboto',
+        fontSize: 16,
+        marginLeft: 5,
+        color: color.textGray,
+        fontWeight: '900'
+    },
+    frameOptionSetting: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 1,
+        marginHorizontal: 20,
+        marginTop: 10,
+        paddingVertical: 15,
+        borderRadius: 15,
+        backgroundColor: color.post
     }
 })
--- screens/Extending/RecipeFunction/hooks/useNewReceipt.js
+++ screens/Extending/RecipeFunction/hooks/useNewReceipt.js
@@ -1,51 +1,51 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import * as ImagePicker from 'expo-image-picker';
 
 function useNewReceipt() {
-    const [uri, setUri] = useState(null)
+  const [uri, setUri] = useState(null)
 
-    const openImagePickerAsync = async () => {
-        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-    
-        if (permissionResult.granted === false) {
-          alert("Permission to access camera roll is required!");
-          return;
-        }
-    
-        let pickerResult = await ImagePicker.launchImageLibraryAsync();
-        console.log(pickerResult);
-        setUri(pickerResult)
-      }
+  const openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
-    
-    const [loading, setLoading] = useState(false)
-    const [processList, setProcessList] = useState([])
-    const handleAddProcess = (process) => {
-        //add process
-        setProcessList([...processList, process])
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
     }
 
-    const [ingredientList, setIngredientList] = useState([])
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+    setUri(pickerResult)
+  }
 
-    const handleAddIngrdient = (nameIngredient, unitIngredient) => {
-        //add ingredient
-        setIngredientList([...ingredientList, unitIngredient + " " + nameIngredient])
-    }
 
-    const handleDeleteProcess = (index) => {
-        let processListTemp = [...processList]
-        processListTemp.splice(index, 1)
-        setProcessList(processListTemp)
-    }
+  const [loading, setLoading] = useState(false)
+  const [processList, setProcessList] = useState([])
+  const handleAddProcess = (process) => {
+    //add process
+    setProcessList([...processList, process])
+  }
 
-    const handleDeleteIngredient = (index) => {
-        let ingredientListTemp = [...ingredientList]
-        ingredientListTemp.splice(index, 1)
-        setIngredientList(ingredientListTemp)
-    }
+  const [ingredientList, setIngredientList] = useState([])
+
+  const handleAddIngrdient = (nameIngredient, unitIngredient) => {
+    //add ingredient
+    setIngredientList([...ingredientList, unitIngredient + " " + nameIngredient])
+  }
+
+  const handleDeleteProcess = (index) => {
+    let processListTemp = [...processList]
+    processListTemp.splice(index, 1)
+    setProcessList(processListTemp)
+  }
+
+  const handleDeleteIngredient = (index) => {
+    let ingredientListTemp = [...ingredientList]
+    ingredientListTemp.splice(index, 1)
+    setIngredientList(ingredientListTemp)
+  }
 
   return {
-      processList, ingredientList, handleAddProcess, handleAddIngrdient, handleDeleteProcess, handleDeleteIngredient, uri, openImagePickerAsync
+    processList, ingredientList, handleAddProcess, handleAddIngrdient, handleDeleteProcess, handleDeleteIngredient, uri, openImagePickerAsync
   }
 
 }
